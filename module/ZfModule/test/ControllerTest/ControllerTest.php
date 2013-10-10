57ce25852ba3a995e69d8257719310b11bb052b6
gerard <gerard@mofo-Easy-Note-MX51.(none)>
2013-10-10 16:49:50 +0200

    db backup

---
+++ module/ZfModule/test/ControllerTest/ControllerTest.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of ControllerTest
+ *
+ * @author gerard
+ */
+class ControllerTest extends PHPUnit_Framework_Testcase 
+{
+    public function testtest()
+    {
+        $this->assertTrue(true);
+    }
+}
